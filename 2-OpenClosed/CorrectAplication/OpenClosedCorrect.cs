9519cbdea4f2aac561e14a8f7c1bbe75b005f870
7lucasdaniel <7lucasdaniel@gmail.com>
2025-05-09 12:00:04 -0300

    OCP correct and violation

--- 2-OpenClosed/CorrectAplication/OpenClosedCorrect.cs
+++ 2-OpenClosed/CorrectAplication/OpenClosedCorrect.cs
@@ -2,7 +2,20 @@ using System;
 
 namespace _2_OpenClosed.CorrectAplication;
 
-public class OpenClosedCorrect
+public static class OpenClosedCorrect
 {
+    public static void Sum(int sum1, int sum2)
+    {
+        Console.WriteLine(sum1 + sum2);
+    }
 
+    public static void Multiplie(int multiplie1, int multiplie2)
+    {
+        Console.WriteLine(multiplie1 * multiplie2);
+    }
+
+    public static void Subtract(int sub1, int sub2)
+    {
+        Console.WriteLine(sub1 - sub2);
+    }
 }
--- 2-OpenClosed/Violation/OpenClosedViolation.cs
+++ 2-OpenClosed/Violation/OpenClosedViolation.cs
@@ -1,8 +1,11 @@
-using System;
-
 namespace _2_OpenClosed.Violation;
 
-public class OpenClosedViolation
+public static class OpenClosedViolation
 {
-
+    public static void CalculateManyThings(int sum1, int sum2, int multiplie1, int multiplie2, int sub1, int sub2)
+    {
+        Console.WriteLine(sum1 + sum2);
+        Console.WriteLine(multiplie1 * multiplie2);
+        Console.WriteLine(sub1 - sub2);
+    }
 }
